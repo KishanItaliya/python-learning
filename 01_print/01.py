cea23cb21142488f07289bc8d767df9ce3e9ea83
Kishankumar Italiya <kishankumar@appscrip.co>
2026-03-15 11:00:05 +0530

    print function

---
+++ 01_print/01.py
@@ -0,0 +1,20 @@
+# Print function in Python
+# print function is used to print the output to the console
+
+# print function with single argument
+print("Hello")
+
+# print function with multiple arguments (arguments are separated by a comma)
+# sep parameter is used to separate the arguments by a string
+print("India", "Love", sep = '/')
+
+# print function with single argument 
+print(5)
+
+print(6.89)
+print(False)
+
+# print function with end parameter (end parameter is used to print the output to the console without a new line)
+print("India", end = " ")
+
+print("South")
